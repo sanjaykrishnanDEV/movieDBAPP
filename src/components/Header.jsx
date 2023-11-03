1f05527114cd34cbfbe1c5063dfeae7e184f958a
sanjaykrishnan <er.sanjaykrishnan@gmail.com>
2023-11-03 11:26:51 +0530

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../components/Header.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
